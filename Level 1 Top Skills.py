56bb3bc97e568abc829a3e9923bfe124edfc1712
My Bitcoin Forecast <103082601+mybitcoinforecast@users.noreply.github.com>
2022-04-12 10:24:48 -0600

    Update Level 1 Top Skills.py

--- Level 1 Top Skills.py
+++ Level 1 Top Skills.py
@@ -17,7 +17,7 @@ if Points > 50:
     BullBear = 'https://i.imgur.com/siL8Hak.png'
 else: BullBear = 'https://i.imgur.com/KyM7b6o.png'
 
-ExpectedReturn = round((-0.1129826611596502 + 0.0021708085870005087 * Points) * 1 * 100, 2)
+PredictedReturn = round((-0.1129826611596502 + 0.0021708085870005087 * Points) * 1 * 100, 2)
 
 print('Bitcoin Bull Bear Forecast')
 display(Image(BullBear))
@@ -25,5 +25,5 @@ print('Points')
 print(Points)
 print('BTC Price')
 print(BTCPrice)
-print('Expected Return')
-print(ExpectedReturn)
+print('Predicted Return 1-Week')
+print(PredictedReturn)
